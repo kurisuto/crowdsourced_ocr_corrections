a5fdfaf493eb580f1bbbc63b5c2a1db5dfd14e61
Sean Crist <kurisuto@panix.com>
2020-06-30 10:10:59 -0400

    Bringing in the client provided for Udacity for the Todo project as the basis for a simple CollaborEdit client

---
+++ client/src/config.ts
@@ -0,0 +1,12 @@
+// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
+const apiId = 'z2v03ngmq4'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+// export const apiEndpoint = 'http://localhost:3003'
+
+
+export const authConfig = {
+  // TODO: Create an Auth0 application and copy values from it into this map
+  domain: 'dev-h7tznt7t.auth0.com',            // Auth0 domain
+  clientId: 'WO1G1GKuArFvqKQD3Te9k1iy9GlTMflm',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
